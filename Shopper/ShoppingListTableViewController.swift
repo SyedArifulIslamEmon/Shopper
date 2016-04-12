61aaa8a67402a6e56e58567a189254d0061aa713
student <student@mh215-02s-imac.local>
2016-04-12 15:09:32 -0400

    Added Delete Capability and Monster Icons

--- Shopper/ShoppingListTableViewController.swift
+++ Shopper/ShoppingListTableViewController.swift
@@ -57,7 +57,7 @@ class ShoppingListTableViewController: UITableViewController {
         let alert = UIAlertController(title: "Add", message: "Shopping List Item", preferredStyle: .Alert)
         
         //action that occurs when add button is pressed
-        
+       
         let addAction = UIAlertAction(title: "Add", style: .Default) { (action) -> Void in
             if let nameTextField = alert.textFields?[0], priceTextField = alert.textFields?[1], quantityTextField = alert.textFields?[2], shoppingListItemEntity = NSEntityDescription.entityForName("ShoppingListItem", inManagedObjectContext: self.managedObjectContext), name = nameTextField.text, price = priceTextField.text, quantity = quantityTextField.text {
                 
@@ -162,26 +162,30 @@ class ShoppingListTableViewController: UITableViewController {
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
     }
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
+
+    
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            let item = shoppingListItems[indexPath.row]
+            
+            managedObjectContext.deleteObject(item)
+            do{
+                try self.managedObjectContext.save()
+            } catch {
+                print("Error saving the managed object context!")
+            }
+            
+            reloadData()
+        }
     }
-    */
-
     /*
     // Override to support rearranging the table view.
     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
